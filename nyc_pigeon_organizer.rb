4ccfc2facc3cb142a417567271cf48ffafa0f5c3
Robert Phillips <rpdecks@gmail.com>
2020-03-21 15:41:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,10 +15,10 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute_key, attribute_value_hash|
     attribute_value_hash.each do | key, names |
       names.each do |name|
-        if new_pigeon_hash.include? !name
+        if !new_pigeon_hash.include? name
         else new_pigeon_hash[name] = {}
         end
-      if !new_pigeon_hash[name].include? !attribute_key
+      if !new_pigeon_hash[name].include? attribute_key
           new_pigeon_hash[name][attribute_key] = []
         end
       end
