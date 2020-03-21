8eba0e569675e86340e6d56ba63a8bc1f97143eb
Robert Phillips <rpdecks@gmail.com>
2020-03-21 15:57:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  data.each do |attribute_key, attribute_value_hash|
+    attribute_value_hash.each do | key, names |
+      names.each do |name|
+        if !new_pigeon_hash.include? name
+          new_pigeon_hash[name] = {}
+        end
+        if !new_pigeon_hash[name].include? attribute_key
+          new_pigeon_hash[name][attribute_key] = []
+        else 
+        end
+      new_pigeon_hash[name][attribute_key] << key.to_s
+      end
+    end
+  end
+  new_pigeon_hash
 end
